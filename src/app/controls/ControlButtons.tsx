65df7e6f08e783663c6e1262947fc0f9a0cb1b69
Clément L <clement.lize1@gmail.com>
2024-01-25 16:37:17 +0100

    feat(controls): added fullscreen button

--- src/app/controls/ControlButtons.tsx
+++ src/app/controls/ControlButtons.tsx
@@ -1,3 +1,5 @@
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
 import LockIcon from '@mui/icons-material/Lock';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { Box, Button, useTheme } from "@mui/material";
@@ -5,13 +7,19 @@ import { Box, Button, useTheme } from "@mui/material";
 interface ControlButtonsProps {
     lockScroll: boolean;
     toggleLockScroll: () => void;
+    fullscreen: boolean;
+    toggleFullscreen: () => void;
     settingsOpened: boolean;
     openSettings: () => void;
 }
 
 const ControlButtons: React.FC<ControlButtonsProps> = (props) => {
 
-    const { lockScroll, toggleLockScroll, settingsOpened, openSettings } = props;
+    const {
+        lockScroll, toggleLockScroll,
+        fullscreen, toggleFullscreen,
+        settingsOpened, openSettings
+    } = props;
 
     const theme = useTheme();
 
@@ -39,6 +47,20 @@ const ControlButtons: React.FC<ControlButtonsProps> = (props) => {
                 onClick={() => openSettings()}
             >
                 <SettingsIcon fontSize="medium" />
+            </Button>
+
+            <Button
+                style={{
+                    backgroundColor: getButtonColor(false),  // Always grey
+                    padding: theme.spacing(1.5),
+                    marginBottom: theme.spacing(1.5),
+                }}
+                onClick={() => toggleFullscreen()}
+            >
+                {fullscreen
+                    ? <FullscreenExitIcon fontSize="medium" />
+                    : <FullscreenIcon fontSize="medium" />
+                }
             </Button>
 
             <Button
--- src/app/controls/Controls.tsx
+++ src/app/controls/Controls.tsx
@@ -15,12 +15,53 @@ const Controls: React.FC<ControlsProps> = (props) => {
 
     const { userData, setUserData, moduleRefs } = props;
 
+    const [fullscreen, setFullscreen] = useState<boolean>(false);
     const [lockScroll, setLockScroll] = useState<boolean>(false);
     const [openSettings, setOpenSettings] = useState<boolean>(false);
 
-    /////////////////
-    // OTHER
-    /////////////////
+    const toggleFullscreen = () => {
+
+        const documentElement = document.documentElement;
+
+        // Want to close fullscreen
+        if (fullscreen) {
+
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            }
+            // @ts-ignore: Safari
+            else if (document.webkitExitFullscreen) {
+                // @ts-ignore: Safari
+                document.webkitExitFullscreen();
+            }
+            // @ts-ignore: IE11
+            else if (document.msExitFullscreen) {
+                // @ts-ignore: IE11
+                document.msExitFullscreen();
+            }
+
+            setFullscreen(false);
+        }
+        // Want to open fullscreen
+        else {
+
+            if (documentElement.requestFullscreen) {
+                documentElement.requestFullscreen();
+            }
+            // @ts-ignore: Safari
+            else if (documentElement.webkitRequestFullscreen) {
+                // @ts-ignore: Safari
+                documentElement.webkitRequestFullscreen();
+            }
+            // @ts-ignore: IE11
+            else if (documentElement.msRequestFullscreen) {
+                // @ts-ignore: IE11
+                documentElement.msRequestFullscreen();
+            }
+
+            setFullscreen(true);
+        }
+    }
 
     return (
 
@@ -35,6 +76,8 @@ const Controls: React.FC<ControlsProps> = (props) => {
             <ControlButtons
                 lockScroll={lockScroll}
                 toggleLockScroll={() => setLockScroll(a => !a)}
+                fullscreen={fullscreen}
+                toggleFullscreen={() => toggleFullscreen()}
                 settingsOpened={openSettings}
                 openSettings={() => setOpenSettings(true)}
             />
