5439c23bf29707c35164031b9fe35cc66315ba8a
clementlize <clement.lize1@gmail.com>
2024-10-22 22:03:00 +0200

    core: updated version to 1.1.1

--- src/app/settings/SettingsAbout.tsx
+++ src/app/settings/SettingsAbout.tsx
@@ -32,7 +32,7 @@ const SettingsAbout: React.FC<SettingsAboutProps> = () => {
             </Box>
 
             <Typography sx={{ marginTop: 2 }}>
-                Version 1.1.0
+                Version 1.1.1
             </Typography>
         </Box>
     );
